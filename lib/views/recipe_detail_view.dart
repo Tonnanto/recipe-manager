6976d7aa6df6ec9644bbae3b7ca6d1b39d8a720b
Anton Stamme <anton@stamme.de>
2021-07-13 19:09:50 +0200

    more recipe detail ui

--- lib/views/recipe_detail_view.dart
+++ lib/views/recipe_detail_view.dart
@@ -24,7 +24,55 @@ class _RecipeDetailState extends State<RecipeDetail> {
     );
   }
 
-  _buildBody() {
+  Widget _buildBody() {
+    return ListView(
+        children: [
+          Image.network("https://picsum.photos/400"),
+          Text(
+            widget.recipe.name,
+            style: TextStyle(fontSize: 32),
+          ),
+          _buildIngredientList()
+        ],
+      );
+  }
 
+  Widget _buildIngredientList() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Ingredients",
+          style: TextStyle(fontSize: 18),
+        ),
+        Container(
+          height: 800,
+          padding: EdgeInsets.all(16),
+          child: ListView.separated(
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (BuildContext context, int index) {
+              return _buildIngredientRow(index);
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return Divider();
+            },
+            itemCount: widget.recipe.ingredients.length,
+          )
+        )
+      ]
+    );
+  }
+
+  Widget _buildIngredientRow(int index) {
+    return Container(
+      height: 32,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(widget.recipe.ingredients[index].name),
+          Text(widget.recipe.ingredients[index].unitAmount.toString()),
+        ],
+      ),
+    );
   }
 }
