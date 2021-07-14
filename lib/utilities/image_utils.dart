569ee02aad20f37daca26adcb73933c686681461
Anton Stamme <anton@stamme.de>
2021-07-14 18:19:43 +0200

    added image utilities

---
+++ lib/utilities/image_utils.dart
@@ -0,0 +1,17 @@
+
+
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
+import 'package:flutter/services.dart';
+
+
+/// Loads an image from assets
+Future<ui.Image> loadUiImage(String assetPath) async {
+  final data = await rootBundle.load(assetPath);
+  final list = Uint8List.view(data.buffer);
+  final completer = Completer<ui.Image>();
+  ui.decodeImageFromList(list, completer.complete);
+  return completer.future;
+}
