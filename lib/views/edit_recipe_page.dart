bbeb8e1d71c7a5b7c57d351b744683667dc22c75
Anton Stamme <anton@stamme.de>
2021-08-25 16:50:39 +0200

    added basic UI for editing / adding ingredients and preparation steps to a recipe

--- lib/views/edit_recipe_page.dart
+++ lib/views/edit_recipe_page.dart
@@ -17,6 +17,8 @@ class EditRecipePage extends StatefulWidget {
 
 class _EditRecipePageState extends State<EditRecipePage> {
   final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
+  final GlobalKey<FormBuilderState> _ingredientsFormKey = GlobalKey<FormBuilderState>();
+  final GlobalKey<FormBuilderState> _preparationStepsFormKey = GlobalKey<FormBuilderState>();
   PageController _pageController =
       PageController(initialPage: 0, keepPage: false);
 
@@ -69,9 +71,9 @@ class _EditRecipePageState extends State<EditRecipePage> {
 
   /// First page that allows user to enter meta data for the recipe
   Widget _buildMetaDataPage() {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: SingleChildScrollView(
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             FormBuilder(
@@ -82,6 +84,9 @@ class _EditRecipePageState extends State<EditRecipePage> {
                   FormBuilderTextField(
                     name: 'name',
                     decoration: InputDecoration(labelText: "Name"),
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.required(context),
+                    ]),
                   ),
                   FormBuilderFilterChip(
                     name: 'recipe_types',
@@ -118,7 +123,8 @@ class _EditRecipePageState extends State<EditRecipePage> {
                                   color: Colors.black12,
                                   child: index == widget.recipe.images.length
                                       ? Icon(Icons.add)
-                                      : Image.memory(widget.recipe.images[index]),
+                                      : Image.memory(
+                                          widget.recipe.images[index]),
                                 ),
                               );
                             },
@@ -132,6 +138,32 @@ class _EditRecipePageState extends State<EditRecipePage> {
                       );
                     },
                   ),
+                  FormBuilderTextField(
+                    name: 'prep_time',
+                    decoration: InputDecoration(
+                      labelText: 'Preparation Time (min)',
+                    ),
+                    valueTransformer: (text) => num.tryParse(text),
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.required(context),
+                      FormBuilderValidators.numeric(context),
+                    ]),
+                    keyboardType: TextInputType.number,
+                  ),
+                  FormBuilderTextField(
+                    name: 'cook_time',
+                    decoration: InputDecoration(
+                      labelText: 'Cooking Time (min)',
+                    ),
+                    valueTransformer: (text) => num.tryParse(text),
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.numeric(context),
+                    ]),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 120,
+                  )
                 ],
               ),
             ),
@@ -143,19 +175,208 @@ class _EditRecipePageState extends State<EditRecipePage> {
 
   /// Second page that allows user to enter all ingredients for the recipe
   Widget _buildIngredientsPage() {
-    return SingleChildScrollView(
-      child: Column(
-        children: [Text("Ingredients")],
-      ),
+    return Column(
+      children: [
+        SizedBox(
+          height: 50,
+          child: Center(
+            child: Text(
+              'Ingredients',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ),
+        Expanded(
+          child: FormBuilder(
+            key: _ingredientsFormKey,
+            child: ListView.separated(
+              itemBuilder: (context, index) {
+                if (index == widget.recipe.ingredients.length) {
+                  return Container(
+                      height: 50,
+                      child: Center(
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            setState(() {
+                              widget.recipe.ingredients
+                                  .add(Ingredient('', UnitAmount(Unit.GRAM, 0)));
+                            });
+                          },
+                          label: Text('Add Ingredient'),
+                          icon: Icon(Icons.add),
+                        ),
+                      ));
+                }
+                return Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  height: 50,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        flex: 8,
+                        child: FormBuilderTextField(
+                          name: 'ingredient $index',
+                          decoration: InputDecoration(
+                            hintText: 'Ingredient',
+                          ),
+                          initialValue: widget.recipe.ingredients[index].name,
+                        ),
+                      ),
+                      Flexible(
+                        flex: 2,
+                        child: FormBuilderTextField(
+                          name: 'amount',
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            hintText: 'amount',
+                          ),
+                          initialValue: widget
+                              .recipe.ingredients[index].unitAmount.amount
+                              .toString(),
+                          onChanged: (value) {
+                            widget.recipe.ingredients[index].unitAmount.amount = _ingredientsFormKey.currentState?.value['amount'];
+                          },
+                        ),
+                      ),
+                      Flexible(
+                        flex: 2,
+                        child: FormBuilderDropdown(
+                          name: widget.recipe.ingredients[index].toString() + ' unit',
+                          items: List<DropdownMenuItem>.generate(
+                              Unit.values.length,
+                              (index) => DropdownMenuItem(
+                                  value: Unit.values[index],
+                                  child: Text(Unit.values[index].shortString()))
+                          ),
+                          initialValue:
+                              widget.recipe.ingredients[index].unitAmount.unit,
+                        ),
+                      ),
+                      Flexible(
+                        child: IconButton(
+                          icon: Icon(Icons.remove_circle),
+                          color: Colors.black26,
+                          onPressed: () {
+                            _removeIngredient(index);
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => SizedBox(
+                height: 8,
+              ),
+              itemCount: widget.recipe.ingredients.length + 1,
+              padding: EdgeInsets.only(bottom: 100),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
   /// Third page that allows user to enter all preparation steps for the recipe
   Widget _buildPreparationStepsPage() {
-    return SingleChildScrollView(
-      child: Column(
-        children: [Text("Preparation steps")],
-      ),
+    return Column(
+      children: [
+        SizedBox(
+          height: 50,
+          child: Center(
+            child: Text(
+              'Preparation Steps',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ),
+        Expanded(
+          child: FormBuilder(
+            key: _preparationStepsFormKey,
+            child: ListView.separated(
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                if (index == widget.recipe.preparationSteps.length) {
+                  return Container(
+                      height: 50,
+                      child: Center(
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            setState(() {
+                              widget.recipe.preparationSteps
+                                  .add(PreparationStep(''));
+                            });
+                          },
+                          label: Text('Add Preparation Step'),
+                          icon: Icon(Icons.add),
+                        ),
+                      ));
+                }
+                return Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 32.0,
+                          height: 32.0,
+                          decoration: new BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                              child: Text((index + 1).toString())
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Flexible(
+                            flex: 8,
+                            child: ConstrainedBox(
+                              constraints: BoxConstraints(
+                                maxHeight: 300
+                              ),
+                              child: FormBuilderTextField(
+                                name: 'prep_step $index',
+                                decoration: InputDecoration(
+                                  hintText: 'Step ' + (index + 1).toString() + '...',
+                                ),
+                                initialValue: widget.recipe.preparationSteps[index].content,
+                                keyboardType: TextInputType.multiline,
+                                minLines: 1,
+                                maxLines: 36,
+                              ),
+                            ),
+                          ),
+                          Flexible(
+                            child: IconButton(
+                              icon: Icon(Icons.remove_circle),
+                              color: Colors.black26,
+                              onPressed: () {
+                                _removePrepStep(index);
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => SizedBox(
+                height: 8,
+              ),
+              itemCount: widget.recipe.preparationSteps.length + 1,
+              padding: EdgeInsets.only(bottom: 100),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -175,13 +396,13 @@ class _EditRecipePageState extends State<EditRecipePage> {
   _removeImage(int index) {
     Widget cancelButton = TextButton(
       child: Text("Cancel"),
-      onPressed:  () {
+      onPressed: () {
         Navigator.of(context).pop();
       },
     );
     Widget continueButton = TextButton(
       child: Text("Remove"),
-      onPressed:  () {
+      onPressed: () {
         Navigator.of(context).pop();
         setState(() {
           widget.recipe.images.removeAt(index);
@@ -204,5 +425,76 @@ class _EditRecipePageState extends State<EditRecipePage> {
         return alert;
       },
     );
+  }
+
+  /// Prompts user to confirm the removal of an ingredient
+  _removeIngredient(int index) {
+    Widget cancelButton = TextButton(
+      child: Text("Cancel"),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+    Widget continueButton = TextButton(
+      child: Text("Remove"),
+      onPressed: () {
+        Navigator.of(context).pop();
+        setState(() {
+          widget.recipe.ingredients.removeAt(index);
+        });
+      },
+    );
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Remove ingredient?"),
+      content: Text("Would you like to remove this ingredient?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  /// Prompts user to confirm the removal of an preparation step
+  void _removePrepStep(int index) {
+    Widget cancelButton = TextButton(
+      child: Text("Cancel"),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+    Widget continueButton = TextButton(
+      child: Text("Remove"),
+      onPressed: () {
+        Navigator.of(context).pop();
+        setState(() {
+          widget.recipe.preparationSteps.removeAt(index);
+        });
+      },
+    );
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Remove Step?"),
+      content: Text("Would you like to remove this preparation step?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+
   }
 }
